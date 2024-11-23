bb93b95c9ad14adc3c3683f5efe409cd193cca1c
kmh <kmh1853@naver.com>
2024-11-23 20:36:31 +0900

    dd

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import ShowList from "../components/ShowList";
-import { Link } from "react-router-dom";
 import myData from "../my_data.json";
 
 const ListPage = () => {
